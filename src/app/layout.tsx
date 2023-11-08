eafb58211b0bf3091addb496b82e12bceba1c52b
penky776 <pohtato@protonmail.com>
2023-11-08 15:02:27 +0500

    add more icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="no-scrollbar">
       <body className={roboto_mono.className}>{children}</body>
     </html>
   )
